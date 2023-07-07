1f765848fea3ed091ebd7bc12e38df2cc5fbdc34
leeonyou <onyou.lee@mincoding.co.kr>
2023-07-07 14:36:44 +0900

    메서드 시간 측정

--- src/main/java/com/project/study/member/MemberRepository.java
+++ src/main/java/com/project/study/member/MemberRepository.java
@@ -1,12 +1,23 @@
 package com.project.study.member;
 
+import com.project.study.utils.annotation.TimeTrace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Slf4j
 public class MemberRepository {
-    public void findAll(){
 
+    @TimeTrace
+    public void findAll(){
+        sleep(1500);
+    }
+
+    private void sleep(int mills) {
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
